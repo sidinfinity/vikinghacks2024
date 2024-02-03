ac3a6b16e2e194c429c33285c82054b9d1599aa1
ryanM154 <55896400+ryanManocha@users.noreply.github.com>
2024-02-03 11:44:08 -0800

    updated main

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 
-import re
-
-def convert_google_sheet_url(url):
-    # Regular expression to match and capture the necessary part of the URL
-    pattern = r'https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?'
-
-    # Replace function to construct the new URL for CSV export
-    # If gid is present in the URL, it includes it in the export URL, otherwise, it's omitted
-    replacement = lambda m: f'https://docs.google.com/spreadsheets/d/{m.group(1)}/export?' + (f'gid={m.group(3)}&' if m.group(3) else '') + 'format=csv'
-
-    # Replace using regex
-    new_url = re.sub(pattern, replacement, url)
-
-    return new_url
-
 # Replace 'your_file.csv' with the path to your CSV file
-
-url ="https://docs.google.com/spreadsheets/d/1ULjkC6x--guX5A4veeJhEzCYuyuG0ryvJHseR9PWIOY/export?format=csv&gid={1ULjkC6x--guX5A4veeJhEzCYuyuG0ryvJHseR9PWIOY}"
-new_url = convert_google_sheet_url(url)
-
+file_path = 'topics.csv'
 
 # Read the CSV file
-df = pd.read_csv(new_url)
-print(df.head())
+df = pd.read_csv(file_path)
+
 # Accessing the data from the columns
 ids = df['id']
 topics = df['topic']
