377c06001c44f0fe0f254fce2526e6e554978ec5
ryanM154 <55896400+ryanManocha@users.noreply.github.com>
2024-02-03 11:06:00 -0800

    index.html

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+from decimal import Decimal
+import pymysql
+pymysql.install_as_MySQLdb()
+from sqlalchemy import DECIMAL
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    name = "product100"
+    print(name)
+    #listOfItems= getLatestProducts()
+    
+    
+    
+    return render_template('index.html', )
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
