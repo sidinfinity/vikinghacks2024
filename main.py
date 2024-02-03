ff8707b253c8b046078b3dd2079cbac554dc54c1
ryanM154 <55896400+ryanManocha@users.noreply.github.com>
2024-02-03 12:33:54 -0800

    fixed topics

--- main.py
+++ main.py
@@ -1,69 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 import pandas as pd
 
-# Replace 'your_file.csv' with the path to your CSV file
-file_path = 'topics.csv'
-
 # Read the CSV file
-df = pd.read_csv(file_path)
-
-# Accessing the data from the columns
-ids = df['id']
-topics = df['topic']
-pdf_dirs = df['pdfdir']
-imgs = df['img']
-
-# If you want to print the data
-print("IDs:", ids)
-print("Topics:", topics)
-print("PDF Directories:", pdf_dirs)
-print("Images:", imgs)
+df = pd.read_csv('topics.csv')
 
 app = Flask(__name__, template_folder='templates')
 
-class Item():      
+class Topic:      
     def __init__(self, id, topic, pdfdir, img):
         self.id = id
         self.topic = topic
         self.pdfdir = pdfdir
         self.img = img
 
-def getTopic(topic_id):
-    listOfItems = products.query.all()
-    
-    for index in range(len(listOfItems)):
-        objectItem = listOfItems[index]
-        if objectItem.id == item_id:
-            priceItem = getPrice(item_id)
-            item = objectItem
-            item.price = priceItem
-    
-    return item
-
-
-
-
-
-#---------------------------------------------
+def getTopics():
+    listTopics = []
+    for i in range(len(df)):
+        row = df.iloc[i]
+        topic = Topic(row['id'], row['topic'], row['pdfdir'], row['img'])
+        listTopics.append(topic)
+    return listTopics
 
 @app.route('/')
 def index():
-    name = "product100"
-    print(name)
-    #listOfItems= getLatestProducts()
-    
-    
-    
-    return render_template('index.html', )
+    listOfTopics = getTopics()
+    return render_template('index.html', items=listOfTopics)
 
-
-@app.route('/topic/<int:item_id>')
+@app.route('/topic/<int:topic_id>')
 def topic_detail(topic_id):
-    print("reached"+ str(item_id))
-    item = getItem(item_id)
-    return render_template('item_detail.html', item=item)
-
-
+    topic = next((t for t in getTopics() if t.id == topic_id), None)
+    if topic:
+        return render_template('topic_detail.html', item=topic)
+    else:
+        return "Topic not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
